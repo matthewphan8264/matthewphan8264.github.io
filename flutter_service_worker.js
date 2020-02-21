d773231750e8365f10e9c118c2acfb059b84cb40
Matthew Phan <matthew.phan@jeevawireless.com>
2020-02-20 16:22:24 -0800

    fixed bugs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,9 +1,12 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "c0def54c56689e27c5ffe2f6bd47637d",
+  "/assets\AssetManifest.json": "a2addee4f1584a4591ba171a1a9a6e95",
 "/assets\assets\icon-jeeva-black.png": "c56dd4be3891052681c6bdf3e2efb663",
 "/assets\assets\icon-null-image.png": "7c841a86b6aef15a58a65274ea9f7178",
+"/assets\assets\pertutorial1.png": "562b160879e88a92555e537ce19e05a4",
+"/assets\assets\pertutorial2.png": "043ec074ad1df695c0109d3af3f1ed95",
+"/assets\assets\pertutorial3.png": "fbabb285a501bd932f556406daedbe81",
 "/assets\assets\test.html": "d41d8cd98f00b204e9800998ecf8427e",
 "/assets\assets\wifirssigood.png": "3522a421930bf8be46cfca041d67a91d",
 "/assets\assets\wifirssigreat.png": "ede632f942c0c3554400f4c4b549164c",
@@ -18,7 +21,7 @@ const RESOURCES = {
 "/assets\packages\font_awesome_flutter\lib\fonts\fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
 "/helper.js": "9b3e366e0b9c8a639c0926558069e5b9",
 "/index.html": "0020a978f4cbca7638adca33a324f62d",
-"/main.dart.js": "8d8c38c44dc269f15d0de76ced29ebaa",
+"/main.dart.js": "0d9d73fdfca90334c42540451c4799bb",
 "/main.dart.js.deps": "af8a0d9838f74fbf9b9a486ee2385809"
 };
 
